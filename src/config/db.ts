9a29b142cee61004116a6c3592931fbd9cb07d2f
Abhay2004Kumar <abs220502@gmail.com>
2025-01-17 16:21:21 +0530

    added shortening api

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    console.log("Database is connected !!!");
+    console.log("Database is connected !!!", process.env.MONGODB_URI);
     
     return mongoose.connect(process.env.MONGODB_URI as string)
 }
